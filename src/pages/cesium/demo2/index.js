0e62392c4f9fa45a156c1b8838803954feab41b8
cc19920212 <995674251@qq.com>
2019-10-08 15:32:48 +0800

    shader_water

--- src/pages/cesium/demo2/index.js
+++ src/pages/cesium/demo2/index.js
@@ -22,8 +22,9 @@ export class cesium_demo2 extends MiddleComponent {
 
   init = ()=>{
     const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNjZiZDQxNS0xNDNlLTRkZDYtOGQ2MC1jZjYzOGI3MDcxYzAiLCJpZCI6MTU5NjYsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjkyOTQwMjZ9.g_e448zt1KzP-BzI82lgrI6CraWgOTHgLe-UaEANwNs'
+    const TOKEN2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwN2YxMWJmMi1lOWI3LTQ3MGUtYTFlYi1mZGQzMzhhMWIxNmYiLCJpZCI6MTU5NjYsInNjb3BlcyI6WyJhc2wiLCJhc3IiLCJhc3ciLCJnYyJdLCJpYXQiOjE1NjkyOTkyMjF9.KLSIwDfkS-pAcHWITpJBb_82OXzt_qadEVXpCZEE06Y';
     window.CESIUM_BASE_URL = "";
-    Cesium.Ion.defaultAccessToken = TOKEN;
+    Cesium.Ion.defaultAccessToken = TOKEN2;
     var viewer = new Cesium.Viewer('cesiumContainer',{
       animation: true, //是否显示动画控件(左下方那个)
       baseLayerPicker: true, //是否显示图层选择控件
@@ -35,16 +36,39 @@ export class cesium_demo2 extends MiddleComponent {
     });
     // viewer.scene.debugShowFramesPerSecond = true;
      viewer.scene.globe.depthTestAgainstTerrain = true;
+     //viewer.trackedEntity = entity1; // 镜头追踪，将镜头固定在对象上
 
-     var entity1 = viewer.entities.add({
-      position: Cesium.Cartesian3.fromDegrees(-103.0, 40.0),
-      ellipse : {
-          semiMinorAxis : 250000.0,
-          semiMajorAxis : 400000.0,
-          material : Cesium.Color.BLUE.withAlpha(0.5)//可设置不同的MaterialProperty
-      }
-    });
-        viewer.trackedEntity = entity1; // 镜头追踪，将镜头固定在对象上
+
+     var scene = viewer.scene;
+        var primitives = scene.primitives;
+        var colors = [
+            new Cesium.Color(77 / 255, 201 / 255, 255 / 255, 1),
+            new Cesium.Color(255 / 255, 201 / 255, 38 / 255, 1),
+            new Cesium.Color(221 / 255, 221 / 255, 221 / 255, 1)
+        ];
+        function add() {
+            for (var i = 0, len = 10; i < len; i++) {
+                var startPoint = Cesium.Cartesian3.fromDegrees(Math.random() * 100, Math.random() * 100);
+                var endPoint = Cesium.Cartesian3.fromDegrees(Math.random() * 100, Math.random() * 100);
+                var positions = Cesium.getLinkedPointList(startPoint, endPoint, 10000, 5000);
+                var color =Cesium.Color.fromRandom({
+                    alpha: 1.0
+                });
+                var glowingLine = viewer.entities.add({
+                    polyline: {
+                        positions: positions,
+                        width: 5,
+                        material:new Cesium.PolylineArrowMaterialProperty(color),
+                        // material: new Cesium.PolylineArrowLinkMaterialProperty({
+                        //     color: color,
+                        //     duration:1000
+                        // }),
+                        extrudedHeight:60000
+                    }
+                });
+            }
+        }
+        // add();
 
   }
   
